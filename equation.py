bbe1f38fde89bacf7cab7db78133c10a71decbb4
B_Gorkovenko <spirit.wood50@gmail.com>
2019-06-05 12:18:58 +0300

    Update equation.py

--- equation.py
+++ equation.py
@@ -1,29 +1,27 @@
 import math
 import unittest
-class TestQuadraticEquation:
+class QEquation:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
-    def test_discriminant(self):
+    def discriminant(self):
         d = ((self.b)**2) - (4 * self.a * self.c)
         return d
-        self.assertEqual(d, 0)
-    def test_root1(self):
-        if self.test_discriminant() < 0:
+    def root1(self):
+        if self.discriminant() < 0:
             return None
         else:
-            r1 = -self.b + math.sqrt(self.test_discriminant()) / (2 * self.a)
+            r1 = (-self.b + math.sqrt(self.discriminant())) / (2 * self.a)
             return r1
-    def test_root2(self):
-        if self.test_discriminant() < 0:
+    def root2(self):
+        if self.discriminant() < 0:
             return None
         else:
-            r2 = -self.b - math.sqrt(self.test_discriminant()) / (2 * self.a)
+            r2 = (-self.b - math.sqrt(self.discriminant())) / (2 * self.a)
             return r2
-        self.assertEqual(r2, -1)
-a = TestQuadraticEquation(1,2,3)
-print(a.test_root1())
-print(a.test_root2())
-print(a.test_discriminant())
 
+a = QEquation(1,-5,4)
+print(a.root1())
+print(a.root2())
+print(a.discriminant())
