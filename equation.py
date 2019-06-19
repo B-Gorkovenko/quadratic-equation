328f2b62ec84f90d8b86d20dde576cb9562a351b
B_Gorkovenko <spirit.wood50@gmail.com>
2019-06-19 15:34:03 +0300

    Update equation.py

--- equation.py
+++ equation.py
@@ -1,12 +1,18 @@
 import math
 
 
-class QEquation:
+class NotQuadratic(Exception):
+    def __init__(self):
+        Exception.__init__(self)
 
+
+class QEquation:
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+        if a == 0:
+            raise NotQuadratic()
 
     def figure_discriminant(self):
         d = ((self.b)**2) - (4 * self.a * self.c)
@@ -16,19 +22,22 @@ class QEquation:
         if self.figure_discriminant() < 0:
             return None
         else:
-            r1 = (-self.b + math.sqrt(self.figure_discriminant())) / (2 * self.a)
+            r1 = (-self.b + math.sqrt(self.figure_discriminant()))\
+                 / (2 * self.a)
             return r1
 
     def figure_root2(self):
         if self.figure_discriminant() < 0:
             return None
         else:
-            r2 = (-self.b - math.sqrt(self.figure_discriminant())) / (2 * self.a)
+            r2 = (-self.b - math.sqrt(self.figure_discriminant())) \
+                 / (2 * self.a)
             return r2
 
 
-a = QEquation(1, -5, 4)
+a = QEquation(5, -5, 4)
 
-print("Первый корень равен: " + str(a.figure_root1()))
-print("Второй корень равен: " + str(a.figure_root2()))
-print("Дискриминант равен: " + str(a.figure_discriminant()))
+if __name__ == '__main__':
+    print("Первый корень равен: " + str(a.figure_root1()))
+    print("Второй корень равен: " + str(a.figure_root2()))
+    print("Дискриминант равен: " + str(a.figure_discriminant()))
